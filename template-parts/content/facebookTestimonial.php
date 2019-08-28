89d9aeb088bb0be96b02760fdadfad1d3cb553af
Daniel Bisett <daniel@austintatiousdesign.co>
2019-08-28 13:14:27 -0500

    testimonials page mod v1

---
+++ template-parts/content/facebookTestimonial.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template part for front-page block1
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+$services_block1_title				= get_field('services_block1_title');
+// insert acf repeater here
+
+?>
+<section id="testimonialGrid">
+
+<div class="fbtest">
+  <!-- insert acf sub-repeater here -->
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjonathon.potter%2Fposts%2F10205803577670035%3A0&width=500" width="500" height="407" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+  </div> 
+  <div class="fbtest">
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdee.a.hinkle%2Fposts%2F10206001663154243%3A0&width=500" width="500" height="407" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+  </div> 
+  <div class="fbtest">
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fannie.burwell%2Fposts%2F1403693236316433%3A0&width=500" width="500" height="425" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+  </div> 
+  <div class="fbtest">
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Flee.williams.102%2Fposts%2F10211565349062510%3A0&width=500" width="500" height="443" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+  </div> 
+  <div class="fbtest">
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmary.k.lenertz%2Fposts%2F10209346928847940%3A0&width=500" width="500" height="317" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+  </div> 
+  <div class="fbtest">
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjacob.elson.3%2Fposts%2F10205428126328755%3A0&width=500" width="500" height="335" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+  </div> 
+
+
+
+</div>
+
+
+  </section>
---
+++ template-parts/content/testimonialQuotes.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for front-page block1
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+$services_block1_title				= get_field('services_block1_title');
+// insert acf repeater here
+
+?>
+
+<section id="testimonialsBlock">
+    <ol class="testimonials-list masonry-list">
+
+            <li>
+            <blockquote class="blockquote blockquote-bubble emphasized">
+                <div class="blockquote-content h3">
+                <div class="blockquote-media">
+                    <img src="https://cdn.dribbble.com/assets/testimonials/haraldur-thorleifsson-3d7c4231b37800e4470a4f38dae39cb869860e7f067972081b311243933b4114.jpg" alt="Haraldur thorleifsson">
+                </div>
+                <p>There is absolutely no doubt in my mind that without Dribbble, I would not have been able to make the jump to Ueno, a digital agency I started in 2014. The work I got through Dribbble made it possible for me to have something to build on. We now have about 45 people on our team, a lot of whom we found and recruited through Dribbble.</p>
+                </div>
+                <cite class="blockquote-cite">
+                <picture>
+    <source srcset="https://cdn.dribbble.com/users/40433/avatars/normal/41b86a77ca368dd4a969397ef39ba9a6.png?1472167036" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
+    <source srcset="https://cdn.dribbble.com/users/40433/avatars/small/41b86a77ca368dd4a969397ef39ba9a6.png?1472167036">
+    <img class="photo" alt="ueno." src="https://cdn.dribbble.com/users/40433/avatars/small/41b86a77ca368dd4a969397ef39ba9a6.png?1472167036">
+    </picture>
+
+                <span><a href="/ueno">ueno.</a></span>
+                </cite>
+            </blockquote>
+            </li>
+
+            <li>
+            <blockquote class="blockquote blockquote-bubble">
+                <p class="blockquote-content">
+                Dribbble has been our primary source when looking for talent, half of our design team was found within the community. Anyone looking for top creatives should be using Dribbble.
+                </p>
+                <cite class="blockquote-cite"><a href="https://dribbble.com/chewy">Chewy</a></cite>
+            </blockquote>
+            </li>
+    </ol>
+</section>
