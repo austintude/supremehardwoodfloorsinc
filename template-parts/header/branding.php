e10e82a3af5188963ea88c324ca083ea76b3de1a
Daniel Bisett <daniel@austintatiousdesign.co>
2019-09-02 09:27:45 -0500

    updated fixes to 24 inch plus monitors v1

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -69,7 +69,7 @@ $hero_image_ipad_landscape_plus = get_sub_field('hero_image_ipad_landscape_plus'
 		<?php
 	} else {
 		?>
-		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 		<?php
 	}
 	?>
