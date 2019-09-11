1e8139836bd1c481e551611bbeb99702194f6a1b
Daniel Bisett <daniel@austintatiousdesign.co>
2019-09-11 16:49:27 -0500

    updated testimonial mobile layout and added schema markup for reviews and ratings

--- template-parts/content/testimonialQuotes.php
+++ template-parts/content/testimonialQuotes.php
@@ -21,6 +21,7 @@ $testimonial_quote = get_sub_field('testimonial_quote');
 $testimonial_author = get_sub_field('testimonial_author');
 $description_of_work = get_sub_field('description_of_work');
 $testimonial_location = get_sub_field('testimonial_location');
+$testimonial_date = get_sub_field('testimonial_date');
 $overall = get_sub_field('overall');
 $price = get_sub_field('price');
 $quality = get_sub_field('quality');
@@ -29,11 +30,13 @@ $punctuality = get_sub_field('punctuality');
 $professionalism = get_sub_field('professionalism');
 ?>
             <li>
-            <blockquote class="blockquote testimonialsCard">
+            <blockquote class="blockquote testimonialsCard" itemscope itemtype="http://schema.org/Review">
                 <div class="testimonialsContent">
-                    <p>
+                    <span itemprop="itemReviewed" itemtype="http://schema.org/localBusiness">
+                    <p itemprop="reviewBody">
                       <?php echo $testimonial_quote; ?>
                     </p>
+</span>
                     <?php if ($description_of_work != null) { ?>
                     <div class="testimonialContentBlocks">
                         <div class="leftBlock">
@@ -47,7 +50,15 @@ $professionalism = get_sub_field('professionalism');
                                     Overall: 
                                     </div>
                                     <div class="rightItem">
-                                        <?php echo $overall; ?>
+                                    <span class="rating" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+                                        <meta itemprop="worstRating" content="0">
+                                            <span itemprop="ratingValue">
+                                            <?php echo $overall; ?>
+                                            </span> / 
+                                            <span itemprop="bestRating">
+                                            <?php echo $overall; ?> 
+                                                </span>
+                                    </span>
                                         </div>
                                 </li>
                                 <li>
@@ -55,7 +66,17 @@ $professionalism = get_sub_field('professionalism');
                                     Price: 
                                     </div>
                                     <div class="rightItem">
+                                    <span class="rating" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+                                        <meta itemprop="worstRating" content="0">
+                                            <span itemprop="ratingValue">
+                                            <?php echo $price; ?>
+                    </span> 
+                    / 
+                                                <span itemprop="bestRating">
+                                                    
                                         <?php echo $price; ?>
+                    </span>
+                    </span>
                     </div>
                                 </li>
                                 <li>
@@ -63,7 +84,15 @@ $professionalism = get_sub_field('professionalism');
                                     Quality: 
                                     </div>
                                     <div class="rightItem">
-                                        <?php echo $quality; ?>
+                                    <span class="rating" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+                                        <meta itemprop="worstRating" content="0">
+                                            <span itemprop="ratingValue">
+                                            <?php echo $quality; ?>
+                    </span> / 
+                    <span itemprop="bestRating">
+                    <?php echo $quality; ?>
+                    </span>
+                    </span>
                                         </div>
                                 </li>
                                 <li>
@@ -71,7 +100,16 @@ $professionalism = get_sub_field('professionalism');
                                     Responsiveness: 
                                     </div>
                                     <div class="rightItem">
+                                    <span class="rating" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+                                        <meta itemprop="worstRating" content="0">
+                                            <span itemprop="ratingValue">
+                                            <?php echo $responsiveness; ?>
+                    </span> / 
+                    <span itemprop="bestRating">
+                                                    
                                         <?php echo $responsiveness; ?>
+                    </span>
+                    </span>
                                         </div>
                                 </li>
                                 <li>
@@ -79,7 +117,16 @@ $professionalism = get_sub_field('professionalism');
                                     Punctuality: 
                                     </div>
                                     <div class="rightItem">
+                                    <span class="rating" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+                                        <meta itemprop="worstRating" content="0">
+                                            <span itemprop="ratingValue">
+                                            <?php echo $punctuality; ?>
+                    </span> / 
+                    <span itemprop="bestRating">
+                                                    
                                         <?php echo $punctuality; ?>
+                    </span>
+                    </span>
                                         </div>
                                 </li>
                                 <li>
@@ -87,7 +134,16 @@ $professionalism = get_sub_field('professionalism');
                                     Professionalism: 
                                     </div>
                                     <div class="rightItem">
+                                    <span class="rating" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+                                        <meta itemprop="worstRating" content="0">
+                                            <span itemprop="ratingValue">
+                                            <?php echo $professionalism; ?>
+                    </span> / 
+                    <span itemprop="bestRating">
+                                                    
                                         <?php echo $professionalism; ?>
+                    </span>
+                    </span>
                                         </div>
                                 </li>
                             </ul>
@@ -97,10 +153,17 @@ $professionalism = get_sub_field('professionalism');
                 </div>
 
                 <cite class="testimonialsQuote">
-                    <?php echo $testimonial_author; ?> - 
+                    <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+                    <?php echo $testimonial_author; ?>
+                    </span> - 
                     <span class="testimonialLocation">
                         <?php echo $testimonial_location; ?>
+                    </span> - 
+                    <meta itemprop="datePublished" content="<?php echo $testimonial_date; ?>">
+                    <span class="testimonialDate">
+                        <?php echo $testimonial_date; ?>
                     </span>
+
                 </cite>
                 
             </blockquote>
