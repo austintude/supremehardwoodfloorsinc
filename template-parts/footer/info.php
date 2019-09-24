25475012a690800b14c92845d42a07e201937ecb
Daniel Bisett <daniel@austintatiousdesign.co>
2019-09-24 08:51:17 -0500

    updated minor tweaks to mobile

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -24,7 +24,7 @@ namespace WP_Rig\WP_Rig;
 </a>
 		</div>
 		<div id="socialIcon2">
-		<a href="#" target="_blank" rel="noopener noreferrer">
+		<a href="https://www.instagram.com/supremehardwoodfloors/" target="_blank" rel="noopener noreferrer">
 		<amp-img 
 				src="<?php echo get_template_directory_uri() ?>/assets/images/instagram-icon-blue.png"
 				width="1"
